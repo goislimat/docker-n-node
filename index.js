517374135293438ff423017501f0e5e94cbbe078
Thiago Lima <thiago.lima@pier.digital>
2019-08-01 19:34:58 -0300

    Revert "Updating package.json test"
    This reverts commit 8b8b0f120602ff9caa6bd9d664c3fdb9abf34ecd.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const _ = require("lodash");
 const app = express();
 const PORT = 3000;
 
 app.get("/", function(req, res) {
-  const who = _.without(["test", "world"], ["test"]);
-  res.send(`Hello ${who[0]}!`);
+  res.send("Hello world!");
 });
 
 app.listen(PORT, function() {
