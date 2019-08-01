8b8b0f120602ff9caa6bd9d664c3fdb9abf34ecd
Thiago Lima <thiago.lima@pier.digital>
2019-08-01 19:25:39 -0300

    Updating package.json test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const _ = require("lodash");
 const app = express();
 const PORT = 3000;
 
 app.get("/", function(req, res) {
-  res.send("Hello world!");
+  const who = _.without(["test", "world"], ["test"]);
+  res.send(`Hello ${who[0]}!`);
 });
 
 app.listen(PORT, function() {
